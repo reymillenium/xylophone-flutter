25a63d055ce1a67ba3e18ceeb5dc687bf48b445a
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-01-23 22:37:04 -0500

    Starting point to migrate this project, prior to version 1.12, to a more modern one (affects the Android version)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +9,58 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('First Noun: ' + nouns.first),
+                  Text('Last Noun: ' + nouns.last),
+                  Text('First adjective: ' + adjectives.first),
+                  Text('Last adjective: ' + adjectives.last),
+                  Text('Adjectives length: ' + adjectives.length.toString()),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      printWords();
+                    },
+                    color: Colors.white,
+                    textColor: Colors.blue.shade900,
+                    padding: const EdgeInsets.all(15.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.play_arrow),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Answer me',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  void printWords() {
+    nouns.take(50).forEach(print);
   }
 }
